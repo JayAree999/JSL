ffdc623b28a99db2b98e4887bbdb3b0e82b91173
JayAree999 <jayyqwe123@gmail.com>
2023-03-12 18:12:56 +0700

    Add Navigate From SavedScreen to LoginScreen and LocationScreen

--- lib/SavedHeader.dart
+++ lib/SavedHeader.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'LocationScreenn.dart';
+import 'LoginScreen.dart';
+
 class SavedHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,28 +12,36 @@ class SavedHeader extends StatelessWidget {
         children: [
           Padding(
             padding: EdgeInsets.only(left: 20, right: 10),
-            child: Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LocationScreen()),
+                );
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-                child: Image.asset(
-                  'assets/images/locimage.png',
-                  height: 50,
-                  width: 50,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: Image.asset(
+                    'assets/images/locimage.png',
+                    height: 50,
+                    width: 50,
+                  ),
                 ),
               ),
             ),
@@ -44,28 +55,36 @@ class SavedHeader extends StatelessWidget {
           ),
           Padding(
             padding: EdgeInsets.only(left: 10, right: 20),
-            child: Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-                child: Image.asset(
-                  'assets/images/backimage.png',
-                  height: 50,
-                  width: 50,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: Image.asset(
+                    'assets/images/backimage.png',
+                    height: 50,
+                    width: 50,
+                  ),
                 ),
               ),
             ),
