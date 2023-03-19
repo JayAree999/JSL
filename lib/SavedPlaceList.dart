eefa6929db43212abeb3a62b6c3e6f7f7425c29a
JayAree999 <jayyqwe123@gmail.com>
2023-03-19 20:28:59 +0700

    Get only places from user instead of displaying everything in documents

--- lib/SavedPlaceList.dart
+++ lib/SavedPlaceList.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'LocationScreenn.dart';
@@ -7,12 +8,27 @@ class SavedPlaceList extends StatefulWidget {
   @override
   _SavedPlaceListState createState() => _SavedPlaceListState();
 }
-
+String currentUserId = '';
 class _SavedPlaceListState extends State<SavedPlaceList> {
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUserId();
+  }
+  void _getCurrentUserId() async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final user = await _auth.currentUser?.uid;
+    if (user != null) {
+      setState(() {
+        currentUserId = user;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('places').snapshots(),
+      stream: FirebaseFirestore.instance.collection('places').where("userId", isEqualTo: currentUserId).snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
@@ -68,3 +84,4 @@ class _SavedPlaceListState extends State<SavedPlaceList> {
     );
   }
 }
+
